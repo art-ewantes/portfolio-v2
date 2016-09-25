3e3299bc4bfca01c7dad760bdb2ca5e25bca051e
art-ewantes <web-rawwwr@gmail.com>
2016-09-25 04:30:01 +0300

    ready singl page portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -113,23 +113,83 @@ gulp.task('build', ['removedist', 'buildhtml', 'imagemin', 'sass', 'libs'], func
 
 });
 
+// gulp.task('deploy', function() {
+
+// 	var conn = ftp.create({
+// 		host:      'ftp.web-rawwwr.com',
+// 		user:      'testFTP@web-rawwwr.com',
+// 		password:  'getthere1995',
+// 		parallel:  10,
+// 		log: gutil.log
+// 	});
+
+// 	var globs = [
+// 	'dist/**',
+// 	'dist/.htaccess',
+// 	];
+// 	return gulp.src(globs, {buffer: false})
+// 	.pipe(conn.dest('/home/wjfrgctj/public_html/wp-content/themes/web-rawwwr'));
+
+// });
+/** Configuration **/
+var user = 'testFTP@web-rawwwr.com';  
+var password = 'getthere1995';  
+var host = 'ftp.web-rawwwr.com';  
+var port = 21;  
+var localFilesGlob = [
+	'dist/assets/**',
+	'dist/*.html',
+];  
+var remoteFolder = '/wp-content/themes/web-rawwwr'
+
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {  
+    return ftp.create({
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        parallel: 5,
+        log: gutil.log
+    });
+}
+
+/**
+ * Deploy task.
+ * Copies the new files to the server
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy`
+ */
 gulp.task('deploy', function() {
 
-	var conn = ftp.create({
-		host:      'hostname.com',
-		user:      'username',
-		password:  'userpassword',
-		parallel:  10,
-		log: gutil.log
-	});
+    var conn = getFtpConnection();
 
-	var globs = [
-	'dist/**',
-	'dist/.htaccess',
-	];
-	return gulp.src(globs, {buffer: false})
-	.pipe(conn.dest('/path/to/folder/on/server'));
+    return gulp.src(localFilesGlob, { base: './dist/', buffer: false })
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files 
+        .pipe( conn.dest( remoteFolder ) )
+    ;
+});
 
+/**
+ * Watch deploy task.
+ * Watches the local copy for changes and copies the new files to the server whenever an update is detected
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy-watch`
+ */
+gulp.task('ftp-deploy-watch', function() {
+
+    var conn = getFtpConnection();
+
+    gulp.watch(localFilesGlob)
+    .on('change', function(event) {
+      console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
+
+      return gulp.src( [event.path], { base: '.', buffer: false } )
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files 
+        .pipe( conn.dest( remoteFolder ) )
+      ;
+    });
 });
 
 gulp.task('clearcache', function () { return cache.clearAll(); });
